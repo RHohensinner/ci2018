7dde7986907889f317236e8831a2a894258469fd
Richard Hohensinner <richard.hohensinner@student.tugraz.at>
2018-04-29 15:37:27 +0200

    added readme.txt and implemented 1.2.a and 1.2.b

---
+++ ass2/readme.txt
@@ -0,0 +1,23 @@
+1) Regression with Neural Networks:
+
+	.1) Simple Regression with Neural Networks:
+		a) Learned function 				DONE
+		b) Variability.. 				DONE
+		c) Varying the number				semi (plot not as intended)
+		d) Variations of MSE				semi (plot not as intended)
+
+		Documentation: 					TODO
+
+	.2) Regularized Neural Networks:
+		a) Weight Decay					DONE
+		b) Early Stopping				done (does what is is supposed to do, SOMETIMES!
+								      3 plots out of ~50 were valid)
+		c) Combining the tricks				TODO
+
+		Documentation:					TODO
+
+2) Face Recognition with Neural Networks:
+
+	.1) Pose Recognition:					TODO
+
+	.2) Face Recognition:					TODO
--- ass2/work/nn_regression.py
+++ ass2/work/nn_regression.py
@@ -233,7 +233,7 @@ def ex_1_1_d(x_train, x_test, y_train, y_test):
                               alpha = alpha, max_iter = 1, random_state = m, tol = 1e-8)
         for n in range(max_iter):
             nn.fit(x_train, y_train)
-            #calculate for every random seed train_mse and test_mse
+            # calculate for every random seed train_mse and test_mse
             train_mse[m][n] = calculate_mse(nn, x_train, y_train)
             test_mse[m][n] = calculate_mse(nn, x_test, y_test)
 
@@ -253,6 +253,31 @@ def ex_1_2_a(x_train, x_test, y_train, y_test):
     :return:
     """
     ## TODO
+    m = 0
+    n = 0
+
+    # declaring variables used in MLP-Regressor
+    hidden_layers = 40
+    alpha = np.array([10**(-8), 10**(-7), 10**(-6), 10**(-5), 10**(-4), 10**(-3), 10**(-2), 10**(-1), 1, 10, 100])
+    random_state = 10
+    activation_mode = 'logistic'
+    solver_mode = 'lbfgs'
+    max_iter = 200
+    train_mse = np.zeros((alpha.size, random_state))
+    test_mse = np.zeros((alpha.size, random_state))
+
+    for m in range(alpha.size):
+        for n in range(random_state):
+
+            nn = MLPRegressor(hidden_layer_sizes = (hidden_layers,), activation = activation_mode, solver = solver_mode, 
+                              alpha = alpha[m], max_iter = max_iter, random_state = n)
+            nn.fit(x_train, y_train)
+            train_mse[m][n] = calculate_mse(nn, x_train, y_train)
+            test_mse[m][n] = calculate_mse(nn, x_test, y_test)
+
+
+    plot_mse_vs_alpha(train_mse, test_mse, alpha)
+
     pass
 
 
@@ -267,6 +292,66 @@ def ex_1_2_b(x_train, x_test, y_train, y_test):
     :return:
     """
     ## TODO
+    m = 0
+    n = 0
+
+    # declaring variables used in MLP-Regressor
+    hidden_layers = 40
+    alpha = 10**(-3)
+    random_state = 10
+    activation_mode = 'logistic'
+    solver_mode = 'lbfgs'
+    max_iter = 2000
+
+    # declaring arrays to save the last iteration, the validation minimum and the test minimum
+    err_last_iter = []
+    err_valid_min = []
+    err_test_min = []
+
+    # same for test/train/valid mse
+    test_mse = np.zeros((max_iter // 20, random_state))
+    train_mse = np.zeros((max_iter // 20, random_state))
+    valid_mse = np.zeros((max_iter // 20, random_state))
+
+    # calculating the permutation
+    permutation = np.random.permutation(x_train.size)
+
+    # declaring new matrices for training and validation
+    x_training_new = np.zeros((x_train.size // 2, 1))
+    y_training_new = np.zeros(x_train.size // 2)
+
+    x_validation_new = np.zeros((x_train.size // 2, 1))
+    y_validation_new = np.zeros(x_train.size // 2)
+
+
+    # fill the new training and validation set using the permutation defined above
+    for m in range(x_train.size // 2):
+        x_training_new[m] = x_train[permutation[m]]
+        y_training_new[m] = y_train[permutation[m]]
+
+        x_validation_new[m] = x_train[permutation[m + (x_train.size // 2)]]
+        y_validation_new[m] = y_train[permutation[m + (y_train.size // 2)]]
+
+    m = 0    
+
+    for m in range(random_state):
+        nn = MLPRegressor(hidden_layer_sizes = (hidden_layers,), activation = activation_mode, solver = solver_mode, 
+                          alpha = alpha, warm_start = True, max_iter = max_iter, random_state = m)
+        for n in range(max_iter):
+
+            # monitor the error every 20 iterations
+            if n % 20 == 0:
+                nn.fit(x_training_new, y_training_new)
+                test_mse[n // 20][m] = calculate_mse(nn, x_test, y_test)
+                train_mse[n // 20][m] = calculate_mse(nn, x_training_new, y_training_new)
+                valid_mse[n // 20][m] = calculate_mse(nn, x_validation_new, y_validation_new)
+
+        err_last_iter.append(test_mse[((n + 1) // 20) - 1][m])
+        err_valid_min.append(test_mse[np.argmin(valid_mse, axis = 0)[m]][m])
+        err_test_min.append(np.min(test_mse, axis = 0)[m])
+
+    plot_bars_early_stopping_mse_comparison(err_last_iter, err_valid_min, err_test_min)
+
     pass
 
 
